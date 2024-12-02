0148c399ccd7769cef3555205f84aee18c47e687
minawashere <minasalama2580@gmail.com>
2024-12-02 03:25:02 +0200

    modified README.md

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # ESP32-odomotery:
-### This projects aims to achieve an odomotery output and mapping.
-- Odometry:
+### This projects aims to achieve the localization and mapping of an autonomous Rover or ROV.
+
+## Localization options: 
+Both options should output a set of data representing the _position_ and _orientation_ 
+in 3d space, _angular_ and _linear_ velocities.
+    
+- **Odometery:** 
+     primary uses wheel encoders combined with IMU data. Used on ground vehicles as dead reckoning is prone to errors from acceleration sensors.
+linear errors occur as a result of uneven or slippery surfaces. Filters and sensor fusion can be applied. 
+
+
+
+- Dead reckoning: relies on sensors that output acceleration, velocity, and direction. Integration drift error accumulates over time. Used in situation where encoders 
+are impractical as water and air vehicles.
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,42 @@
 #include <Arduino.h>
 #include <ESP32Encoder.h>
-#include <driver/pcnt.h>
 
-ESP32Encoder encoderRight;
-ESP32Encoder encoderLeft;
+using encoderPin = uint8_t;
+enum EncoderMode {HALF_QUAD, FULL_QUAD, SINGLE};
 
+struct Differential_drive final : ESP32Encoder{
+    Differential_drive(const encoderPin pA, const encoderPin pB,
+        const EncoderMode encoderMode = FULL_QUAD){
+
+        useInternalWeakPullResistors = puType::up;
+
+        switch (encoderMode)
+        {
+        case EncoderMode::FULL_QUAD:
+            attachFullQuad(pA, pB);
+            break;
+        case EncoderMode::HALF_QUAD:
+            attachHalfQuad(pA, pB);
+            break;
+        case EncoderMode::SINGLE:
+            attachSingleEdge(pA, pB);
+        }
+        clearCount();
+    }
+    ~Differential_drive()= default;
+};
+
+auto motorRight = Differential_drive(27, 26);
+auto motorLeft = Differential_drive(12, 14);
 
 void setup(){
-
     Serial.begin(115200);
 
-    ESP32Encoder::useInternalWeakPullResistors = puType::up;
-
-    encoderRight.attachFullQuad(27, 26);
-    encoderLeft.attachFullQuad(25, 33);
-
-
-    encoderRight.clearCount();
-    encoderLeft.clearCount();
-
-    if (encoderRight.isAttached() && encoderLeft.isAttached())
+    if (motorRight.isAttached() && motorLeft.isAttached())
     Serial.println("Encoders attached");
 
 }
 
 void loop(){
-    Serial.println(String(encoderLeft.getCount()) + " / " + String(encoderRight.getCount()));
+    Serial.println(String(motorLeft.getCount()) + " / " + String(motorRight.getCount()));
 }
